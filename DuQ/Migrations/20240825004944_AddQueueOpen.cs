c46b8dd62e6aea7939bf384e8edaae1e99b2036f
dropbeardevs <110198441+dropbeardevs@users.noreply.github.com>
2024-08-25 22:44:38 -0700

    DB stuff

---
+++ DuQ/Migrations/20240825004944_AddQueueOpen.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuQ.Migrations.Duq
+{
+    /// <inheritdoc />
+    public partial class AddQueueOpen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_open",
+                schema: "duqueue",
+                table: "du_queue_wait_times",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_open",
+                schema: "duqueue",
+                table: "du_queue_wait_times");
+        }
+    }
+}
---
+++ DuQ/Migrations/20240825034537_AddDeptUrl.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuQ.Migrations.Duq
+{
+    /// <inheritdoc />
+    public partial class AddDeptUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "dept_url",
+                schema: "duqueue",
+                table: "du_queue_wait_times",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "du_queue_wait_times",
+                columns: new[] { "id", "location", "wait_time", "queue_no_students", "modified_utc", "is_open", "dept_url"},
+                values: new object[]
+                        { Guid.NewGuid(), "Hornets Resource Center", 0, 0, DateTime.UtcNow, true, "https://fc.xtours.io/#WC-1" }
+            );
+
+            migrationBuilder.InsertData(
+                table: "du_queue_wait_times",
+                columns: new[] { "id", "location", "wait_time", "queue_no_students", "modified_utc", "is_open", "dept_url"},
+                values: new object[]
+                        { Guid.NewGuid(), "Admissions & Records Campus ID", 0, 0, DateTime.UtcNow, true, "https://fc.xtours.io/#2000" }
+            );
+
+            migrationBuilder.InsertData(
+                table: "du_queue_wait_times",
+                columns: new[] { "id", "location", "wait_time", "queue_no_students", "modified_utc", "is_open", "dept_url"},
+                values: new object[]
+                        { Guid.NewGuid(), "Admissions & Records Window", 0, 0, DateTime.UtcNow, true, "https://fc.xtours.io/#2000" }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dept_url",
+                schema: "duqueue",
+                table: "du_queue_wait_times");
+        }
+    }
+}
